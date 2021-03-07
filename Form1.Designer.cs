2752eb8621352f5da846b4a30c4aa4a68fc4427a
Emran147 <75335633+Emran147@users.noreply.github.com>
2021-03-07 18:23:10 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace Qedan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.txtDesc = new System.Windows.Forms.TextBox();
+            this.ItemPrice = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBarcode = new System.Windows.Forms.TextBox();
+            this.lblViewr = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.btnTotalPrice = new System.Windows.Forms.Button();
+            this.btnbuy = new System.Windows.Forms.Button();
+            this.RbtnNormal = new System.Windows.Forms.RadioButton();
+            this.RbtnVip = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(11, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Item Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(148, 70);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 3;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(148, 109);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(100, 20);
+            this.txtPrice.TabIndex = 4;
+            // 
+            // txtDesc
+            // 
+            this.txtDesc.Location = new System.Drawing.Point(148, 146);
+            this.txtDesc.Name = "txtDesc";
+            this.txtDesc.Size = new System.Drawing.Size(100, 20);
+            this.txtDesc.TabIndex = 5;
+            // 
+            // ItemPrice
+            // 
+            this.ItemPrice.AutoSize = true;
+            this.ItemPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.ItemPrice.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ItemPrice.Location = new System.Drawing.Point(12, 109);
+            this.ItemPrice.Name = "ItemPrice";
+            this.ItemPrice.Size = new System.Drawing.Size(80, 20);
+            this.ItemPrice.TabIndex = 6;
+            this.ItemPrice.Text = "Item Price";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(11, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Item Descreption";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(148, 239);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(100, 31);
+            this.btnAdd.TabIndex = 8;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(11, 190);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Item BarCode";
+            // 
+            // txtBarcode
+            // 
+            this.txtBarcode.Location = new System.Drawing.Point(148, 190);
+            this.txtBarcode.Name = "txtBarcode";
+            this.txtBarcode.Size = new System.Drawing.Size(100, 20);
+            this.txtBarcode.TabIndex = 10;
+            // 
+            // lblViewr
+            // 
+            this.lblViewr.AutoSize = true;
+            this.lblViewr.Location = new System.Drawing.Point(56, 285);
+            this.lblViewr.Name = "lblViewr";
+            this.lblViewr.Size = new System.Drawing.Size(0, 13);
+            this.lblViewr.TabIndex = 11;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(668, 195);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(58, 13);
+            this.lblTotal.TabIndex = 14;
+            this.lblTotal.Text = "Total Price";
+            // 
+            // btnTotalPrice
+            // 
+            this.btnTotalPrice.Location = new System.Drawing.Point(617, 162);
+            this.btnTotalPrice.Name = "btnTotalPrice";
+            this.btnTotalPrice.Size = new System.Drawing.Size(147, 23);
+            this.btnTotalPrice.TabIndex = 15;
+            this.btnTotalPrice.Text = "Check The Total Price";
+            this.btnTotalPrice.UseVisualStyleBackColor = true;
+            this.btnTotalPrice.Click += new System.EventHandler(this.btnTotalPrice_Click);
+            // 
+            // btnbuy
+            // 
+            this.btnbuy.Location = new System.Drawing.Point(651, 220);
+            this.btnbuy.Name = "btnbuy";
+            this.btnbuy.Size = new System.Drawing.Size(75, 23);
+            this.btnbuy.TabIndex = 16;
+            this.btnbuy.Text = "Buy";
+            this.btnbuy.UseVisualStyleBackColor = true;
+            this.btnbuy.Click += new System.EventHandler(this.btnbuy_Click);
+            // 
+            // RbtnNormal
+            // 
+            this.RbtnNormal.AutoSize = true;
+            this.RbtnNormal.Checked = true;
+            this.RbtnNormal.Location = new System.Drawing.Point(630, 55);
+            this.RbtnNormal.Name = "RbtnNormal";
+            this.RbtnNormal.Size = new System.Drawing.Size(58, 17);
+            this.RbtnNormal.TabIndex = 17;
+            this.RbtnNormal.TabStop = true;
+            this.RbtnNormal.Text = "Normal";
+            this.RbtnNormal.UseVisualStyleBackColor = true;
+            // 
+            // RbtnVip
+            // 
+            this.RbtnVip.AutoSize = true;
+            this.RbtnVip.Location = new System.Drawing.Point(630, 78);
+            this.RbtnVip.Name = "RbtnVip";
+            this.RbtnVip.Size = new System.Drawing.Size(40, 17);
+            this.RbtnVip.TabIndex = 18;
+            this.RbtnVip.Text = "Vip";
+            this.RbtnVip.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.RbtnVip);
+            this.Controls.Add(this.RbtnNormal);
+            this.Controls.Add(this.btnbuy);
+            this.Controls.Add(this.btnTotalPrice);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblViewr);
+            this.Controls.Add(this.txtBarcode);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ItemPrice);
+            this.Controls.Add(this.txtDesc);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.TextBox txtDesc;
+        private System.Windows.Forms.Label ItemPrice;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBarcode;
+        private System.Windows.Forms.Label lblViewr;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Button btnTotalPrice;
+        private System.Windows.Forms.Button btnbuy;
+        private System.Windows.Forms.RadioButton RbtnNormal;
+        private System.Windows.Forms.RadioButton RbtnVip;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Qedan
+{
+    public partial class Form1 : Form
+    {
+        string inputfile = @"../../file1.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            int flag = 0;
+            foreach (var line in File.ReadAllLines(inputfile))//עוברים על כל שורה 
+            {
+                string[] inputs = line.Split(',');//מחלקים לפי הפסיק
+                if(inputs[3]==txtBarcode.Text)
+                {
+                    MessageBox.Show("this Barcode in use");
+                    flag = 1;
+                }
+            }
+
+            if (txtBarcode.Text==""||txtDesc.Text==""||txtName.Text==""||txtPrice.Text=="")
+            {
+                MessageBox.Show("Fill All the inputs");
+                flag = 1;
+            }
+           if(flag==0)
+            {
+                //מוספים את פרטי המוצר בנוסף פסיק ביניהם
+                File.AppendAllText(inputfile, txtName.Text + "," + txtPrice.Text + "," + txtDesc.Text + "," + txtBarcode.Text + "\n");
+                //משתנה שמכיל הטקסת שבקובץ
+                String input = File.ReadAllText(inputfile);
+                //הצגה
+                lblViewr.Text = input;
+                MessageBox.Show("item added successfully");
+            }
+        }
+
+        private void btnTotalPrice_Click(object sender, EventArgs e)
+        {
+            int sum = 0;//משתנה להמחיר הסופי
+            foreach (var line in File.ReadAllLines(inputfile))//עוברים על המחירים לפי שורה
+            {
+                string[] inputs = line.Split(',');
+                sum += int.Parse(inputs[1]);
+            }
+            if (RbtnVip.Checked)
+                sum = sum -(sum*10/100);
+            lblTotal.Text = sum.ToString();
+        }
+
+ 
+
+        private void btnbuy_Click(object sender, EventArgs e)
+        {
+             File.WriteAllText(inputfile, "");//איפוס file
+            String input = File.ReadAllText(inputfile);  //משתנה שמכיל הטקסת שבקובץ
+            lblViewr.Text = input;//הצגה
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PictureBox pb = new PictureBox();
+            pb.Location = new Point(250, 20);
+            pb.Size = new Size(800, 1350);
+            pb.Image = Image.FromFile(@"../../33.JPEG");
+            pb.Visible = true;
+            this.Controls.Add(pb);
+        }
+
+        
+    }
+}
